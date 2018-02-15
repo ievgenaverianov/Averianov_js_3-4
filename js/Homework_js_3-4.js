78a4af274f263ddde70e53f4bba8b712f41e6185
Ievgen Averianov <averyanovsumy@gmail.com>
2018-02-15 02:45:47 +0200

    Rework after code review

--- js/Homework_js_3-4.js
+++ js/Homework_js_3-4.js
@@ -8,35 +8,48 @@ var oTest = {
     document.body.appendChild(oTestTitle);
   },
   /**
-   * Method that creates <ul> tags - the questions of the test
+   * Method that creates the questions of the test
    */
-  createQuestion: function () {
-    for (var i = 1; i <= 3; i++)
-    {
-      var oTestQuestion = document.createElement('ul');
-      oTestQuestion.innerHTML = i + '. Вопрос №' + i;
-      document.body.appendChild(oTestQuestion);
-      oTestQuestion.classList.add('list-group');
+  createQuestions: function () {
+    var oTestQuestions = document.createElement('ol');
+    document.body.appendChild(oTestQuestions);
+    for (var i = 1; i <= 3; i++) {
+      var oTestQuestionsItem = document.createElement('li');
+      oTestQuestionsItem.innerHTML = 'Вопрос №' + i;
+      oTestQuestions.appendChild(oTestQuestionsItem);
     }
   },
   /**
-   * Method that creates <li> tags - the answers of the test with checkboxes
+   * Method that creates the answers of the test with checkboxes
    */
   createAnswers: function () {
-    var aQuestions = document.getElementsByTagName('ul');
-    for (var i = 0; i <= 2; i++) {
-      for (var k = 1; k <= 3; k++) {
-      var oTestAnswer = document.createElement('li');
-      var oTestAnswerCheckbox = document.createElement('input');
-      var oTestAnswerText = document.createElement('label');
-      aQuestions[i].appendChild(oTestAnswer);
-      oTestAnswer.appendChild(oTestAnswerCheckbox);
-      oTestAnswer.appendChild(oTestAnswerText);
-      oTestAnswerCheckbox.setAttribute('type', 'checkbox');
-      oTestAnswerCheckbox.setAttribute('id', 'test_answer' + i +k);
-      oTestAnswerCheckbox.classList.add('form-check-input');
-      oTestAnswerText.innerHTML = 'Вариант ответа №' + k;
-      oTestAnswerText.setAttribute('for', 'test_answer' + i + k);
+    var aQuestions = document.getElementsByTagName('li');
+    for (var i = 0; i < aQuestions.length; i++) {
+      var oQuestion = aQuestions[i];
+      var oAnswersList = document.createElement('ul');
+      oQuestion.appendChild(oAnswersList);
+    }
+
+    var aAnswers = document.getElementsByTagName('ul');
+    console.log(aAnswers);
+    for (i = 0; i < aAnswers.length; i++) {
+      for (var k = 0; k < 3; k++) {
+        var oAnswer = aAnswers[i];
+        var oTestAnswer = document.createElement('li');
+        oAnswer.appendChild(oTestAnswer);
+
+        var sTestAnswerId = 'test_answer' + i + k;
+
+        var oTestAnswerCheckbox = document.createElement('input');
+        oTestAnswerCheckbox.setAttribute('type', 'checkbox');
+        oTestAnswerCheckbox.setAttribute('id', sTestAnswerId);
+        oTestAnswerCheckbox.classList.add('form-check-input', 'test-answer-checkbox');
+        oTestAnswer.appendChild(oTestAnswerCheckbox);
+
+        var oTestAnswerText = document.createElement('label');
+        oTestAnswerText.innerHTML = 'Вариант ответа №' + (k + 1);
+        oTestAnswerText.setAttribute('for', sTestAnswerId);
+        oTestAnswer.appendChild(oTestAnswerText);
       }
     }
   },
@@ -46,15 +59,15 @@ var oTest = {
   createButton: function () {
     var oTestButton = document.createElement('button');
     oTestButton.innerHTML = 'Проверить мои результаты';
+    oTestButton.classList.add('btn', 'btn-primary', 'centered-button');
     document.body.appendChild(oTestButton);
-    oTestButton.classList.add('btn', 'btn-primary');
   },
   /**
    * Method that includes all previous methods and creates the whole test
    */
   createTest: function () {
     this.createTitle();
-    this.createQuestion();
+    this.createQuestions();
     this.createAnswers();
     this.createButton();
   }
